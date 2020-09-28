0c33afa63a89b9bee62a97076b2f4b8618bddd26
Mohammad-Agha <hamadaagha123@gmail.com>
2020-09-28 10:24:30 +0300

    Finished JS-01 Step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name = prompt("Enter your name: ");
+let surname = prompt("Enter your surname: ");
+let city = prompt("Enter your city: ");
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
