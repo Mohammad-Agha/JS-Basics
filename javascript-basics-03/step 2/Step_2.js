7be58572400ba803f3fb13ba5038900fff80ceae
Mohammad-Agha <hamadaagha123@gmail.com>
2020-09-28 20:11:46 +0300

    Updated JS-03 Step2

--- javascript-basics-03/step 2/Step_2.js
+++ javascript-basics-03/step 2/Step_2.js
@@ -1,11 +1,16 @@
-let anchorShow = document.querySelector("#show");
-let anchorHide = document.querySelector("#hide");
+let anchors = document.querySelectorAll("a");
 let paragraph = document.querySelector("#texte");
 
-anchorShow.addEventListener("click", () => {
-  paragraph.style = "display: normal";
-});
+for (let i = 0; i < anchors.length; i++) {
+  anchors[i].addEventListener("click", () => {
+    hideShowText(anchors[i].id, paragraph);
+  });
+}
 
-anchorHide.addEventListener("click", () => {
-  paragraph.style = "display: none";
-});
+const hideShowText = (id, paragraph) => {
+  if (id == "show") {
+    paragraph.style = "display: normal";
+  } else if (id == "hide") {
+    paragraph.style = "display: none";
+  }
+};
