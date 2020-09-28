f07b38ab2bbf4a2331b500dad0dcbdd9aa4213ab
Mohammad-Agha <hamadaagha123@gmail.com>
2020-09-28 12:09:01 +0300

    Finished JS-02 Step6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,12 @@
+let images = document.querySelectorAll("img");
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("mouseover", () => {
+    let j = i + 1;
+    images[i].src = "images/image" + j + "_2.jpg";
+  });
+  images[i].addEventListener("mouseout", () => {
+    let j = i + 1;
+    images[i].src = "images/image" + j + ".jpg";
+  });
+}
